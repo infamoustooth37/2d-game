60655ddbe88f3a604f2742d008f1a7a7df214682
infamoustooth37 <57527041+infamoustooth37@users.noreply.github.com>
2020-07-31 23:07:33 +0100

    Test 1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb; 
     private void Update()
     {
-        
+
+        // TEST 123
         if(Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
